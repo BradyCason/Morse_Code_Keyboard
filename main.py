58b257797e6fe2f66714c25dfbbfdad47501ff9e
BradyCason <bradyrcason@gmail.com>
2024-10-10 11:01:16 -0700

    Added Morse Code Dictionary

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ import threading
 key = "alt"
 dot_time = 0.1
 
+code = {
+    ".-": "a", "-...": "b", "-.-.": "c", "-..": "d", ".": "e", "..-.": "f", "--.": "g", 
+    "....": "h", "..": "i", ".---": "j", "-.-": "k", ".-..": "l", "--": "m", "-.": "n", 
+    "---": "o", ".--.": "p", "--.-": "q", ".-.": "r", "...": "s", "-": "t", "..-": "u", 
+    "...-": "v", ".--": "w", "-..-": "x", "-.--": "y", "--..": "z", ".----": "1", 
+    "..---": "2", "...--": "3", "....-": "4", ".....": "5", "-....": "6", "--...": "7", 
+    "---..": "8", "----.": "9", "-----": "0"
+}
+
 pressed_time = 0
 char_timer = None                   
 char_timer_running = False
@@ -14,15 +23,18 @@ key_pressed = False
 cur_str = ""
 
 def dot_or_dash(t):
-    # Returns ".", "-", or " " depending on time length
+    # Returns "." or "-" depending on time length
     global dot_time
 
     if t < 2 * dot_time:
         return "."
     return "-"
 
-def get_char(str):
-    return "a"
+def get_char(s):
+    global code
+    if s in code.keys():
+        return code[s]
+    return ""
 
 def on_space_timer_end():
     keyboard.write(" ")
@@ -38,7 +50,7 @@ def on_key_release(event):
     global pressed_time, char_timer, char_timer_running, space_timer, space_timer_running, key_pressed, cur_str
     key_pressed = False
 
-    char_timer = threading.Timer(dot_time * 4, on_char_timer_end)
+    char_timer = threading.Timer(dot_time * 3, on_char_timer_end)
     char_timer.start()
     char_timer_running = True
     space_timer = threading.Timer(dot_time * 7, on_space_timer_end)
@@ -48,7 +60,6 @@ def on_key_release(event):
     new_char = dot_or_dash(time.time() - pressed_time)
     keyboard.write(new_char)
     cur_str += new_char
-    print(cur_str)
     
 def on_key_press(event):
     global pressed_time, char_timer, char_timer_running, space_timer, space_timer_running, key_pressed
